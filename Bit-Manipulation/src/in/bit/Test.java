785c40da4032b60c8d95924388e9e8347bb4ba56
Sunny-jsg <tech.sunny1@gmail.com>
2025-03-10 15:34:35 +0530

    commit

---
+++ Bit-Manipulation/src/in/bit/Test.java
@@ -0,0 +1,14 @@
+package in.bit;
+
+public class Test {
+	public static void main(String[] args) {
+		int a =5;
+		int b =9;
+		System.out.println(a&b);//1
+		System.out.println(a|b);//13
+		System.out.println(a^b);//12
+		System.out.println(a>>1);//2
+		System.out.println(a<<1);//10
+	}
+
+}
